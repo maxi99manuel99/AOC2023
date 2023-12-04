ac2bd6529e835b29d4be2c2dd03a1cb856205b5d
Manuel <manuel.trageser789@googlemail.com>
2023-12-04 23:29:59 +0100

    finished puzzle04 part2

--- puzzle04/scratchcards.py
+++ puzzle04/scratchcards.py
@@ -5,15 +5,30 @@ class Scratchcard():
         winning_string, drawn_string = card_string.split("|")
         self.winning_numbers, self.drawn_numbers = np.array(winning_string.split(), int), np.array(drawn_string.split(), int)
     
-    def calculate_points(self) -> int:
+    def calculate_num_winnings_and_points(self) -> (int, int):
+        """
+        Returns the count of winning numbers of this Scratchcard and the points
+        it is worth
+        """
         num_intersects = len(np.intersect1d(self.winning_numbers, self.drawn_numbers))
-        return  int(2**(num_intersects-1))
+        return  num_intersects, int(2**(num_intersects-1))
 
 if __name__ == "__main__":
+    
     with open("input.txt") as fp:
+        copies_per_card = {}
         point_sum = 0
+        total_cards = 0
+        card_idx = 0
         while line:= fp.readline():
             sratch_card = Scratchcard(line.split(":")[1])
-            point_sum += sratch_card.calculate_points()
+            num_winnings, points = sratch_card.calculate_num_winnings_and_points()
+            point_sum += points
+            num_this_card = 1 + copies_per_card.get(card_idx, 0)
+            for i in range(1, num_winnings+1):
+                copies_per_card[card_idx+i] = copies_per_card.get(card_idx+i, 0) + num_this_card
+            total_cards  += num_this_card
+            card_idx += 1
         print(f"Part 1 Result: {point_sum}")
+        print(f"Part 2 Resulst: {total_cards}")
             
