ac4b8195f6664887e3ef450992e29ff79e27066f
Manuel <manuel.trageser789@googlemail.com>
2023-12-06 13:25:22 +0100

    updated documentation

--- puzzle06/wait_for_it_math.py
+++ puzzle06/wait_for_it_math.py
@@ -9,11 +9,13 @@ class Race():
         """
         Calculates the ways to beat the race using the quadratic equation
         0 = holding_time^2 - holding_times*self.time + distance.
-        When inserting self.distance_record to distance the number
-        of ints between x2 and x1 will be the amount of ways to beat the self.distance_record
         """
         delta = np.sqrt((self.time**2 - 4*self.distance_record))
 
+        # When inserting self.distance_record to distance and rounding down the lower bound (x1)
+        # and rounding up the upper bound (x2), this means that x1 and x2 do not beat the record
+        # but all ints between x2 and x1 will beat the self.distance_record. Thats why we calculate
+        # x2 - x1 - 1
         x1 = int(np.floor((time - delta) / 2))
         x2 = int(np.ceil((time + delta) / 2))
 
