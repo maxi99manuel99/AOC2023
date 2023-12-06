c838b96f1fc330cbd023f1433ac95ce95b1b8b3d
Manuel <manuel.trageser789@googlemail.com>
2023-12-06 11:35:12 +0100

    numpy improvement

--- puzzle06/wait_for_it.py
+++ puzzle06/wait_for_it.py
@@ -5,8 +5,10 @@ class Race():
         self.time = time
         self.distance_record = distance_record
 
-    def calculate_ways_to_beat(self):
-        return [(holding_time, holding_time*(self.time - holding_time)) for holding_time in range(1,self.time) if holding_time*(self.time - holding_time) > self.distance_record]
+    def calculate_number_ways_to_beat(self):
+        holding_times = np.arange(1, self.time)
+        distances = holding_times * (self.time - holding_times)
+        return np.sum(distances > self.distance_record)
 
 
 if __name__ == "__main__":
@@ -19,13 +21,13 @@ if __name__ == "__main__":
         product_number_ways_to_beat = 1
         for time, distance in zip(times, distances):
             race = Race(time, distance)
-            product_number_ways_to_beat *= len(race.calculate_ways_to_beat())
+            product_number_ways_to_beat *= race.calculate_number_ways_to_beat()
         print(f"Part 1 Result: {product_number_ways_to_beat}")
         
         actual_race_time = int(''.join(map(str, times)))
         actual_distance = int(''.join(map(str, distances)))
         actual_race = Race(actual_race_time, actual_distance)
-        print(f"Part 2 Result: {len(actual_race.calculate_ways_to_beat())}")
+        print(f"Part 2 Result: {actual_race.calculate_number_ways_to_beat()}")
 
 
         
