440e9c623151d9030e880ba97e247641a659f58b
Manuel <manuel.trageser789@googlemail.com>
2023-12-18 22:29:58 +0100

    puzzle17 remove path tracking

--- puzzle17/clumsy_crucible.py
+++ puzzle17/clumsy_crucible.py
@@ -10,11 +10,10 @@ class DIRECTION(Enum):
     UNDEFINED = 4
 
 class QueueObject():
-    def __init__(self, position: tuple[int, int], heat_loss: int, moving_direction: tuple[DIRECTION, int], path) -> None:
+    def __init__(self, position: tuple[int, int], heat_loss: int, moving_direction: tuple[DIRECTION, int]) -> None:
         self.position = position
         self.heat_loss = heat_loss
         self.moving_direction = moving_direction
-        self.path = path
     
     def __lt__(self, other):
         return self.heat_loss < other.heat_loss
@@ -28,8 +27,8 @@ class HeatLossMap():
         priority_queue = []
         visited = set()
 
-        for neighbor_pos, neighbor_direction in self.get_neighbors(start_block, DIRECTION.UNDEFINED, 0):
-            neighbor_obj = QueueObject(neighbor_pos, self.heat_map[neighbor_pos], neighbor_direction, [neighbor_pos])
+        for neighbor_pos, (neighbor_direction, neighbor_streak) in self.get_neighbors(start_block, DIRECTION.UNDEFINED, 0):
+            neighbor_obj = QueueObject(neighbor_pos, self.heat_map[neighbor_pos], (neighbor_direction, neighbor_streak))
             heapq.heappush(priority_queue, neighbor_obj)
 
         while priority_queue:
@@ -43,9 +42,9 @@ class HeatLossMap():
             
             visited.add((pos, prev_direction, direction_streak))
 
-            for neighbor_pos, neighbor_direction in self.get_neighbors(pos, prev_direction, direction_streak):
-                if (neighbor_pos, neighbor_direction[0], neighbor_direction[1]) not in visited:
-                    neighbor_obj = QueueObject(neighbor_pos, heat_loss + self.heat_map[neighbor_pos], neighbor_direction, curr_obj.path + [neighbor_pos])
+            for neighbor_pos, (neighbor_direction, neighbor_streak) in self.get_neighbors(pos, prev_direction, direction_streak):
+                if (neighbor_pos, neighbor_direction, neighbor_streak) not in visited:
+                    neighbor_obj = QueueObject(neighbor_pos, heat_loss + self.heat_map[neighbor_pos], (neighbor_direction, neighbor_streak))
                     heapq.heappush(priority_queue, neighbor_obj)
 
     def get_neighbors(self, pos: tuple[int, int], prev_direction: DIRECTION, direction_streak: int) -> list[tuple[tuple[int, int], tuple[DIRECTION, int]]]:
