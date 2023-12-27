483c85601d4cdb6d537683b864d192dc53f1b4c6
Manuel <manuel.trageser789@googlemail.com>
2023-12-27 10:42:47 +0100

    fixed false function call and formatting

--- puzzle24/hailstones.py
+++ puzzle24/hailstones.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from z3 import IntVector, Solver
 
+
 class HailStone():
     def __init__(self, pos: tuple[int, int, int], velocity: tuple[int, int, int]) -> None:
         self.x = pos[0]
@@ -9,7 +10,7 @@ class HailStone():
         self.vx = velocity[0]
         self.vy = velocity[1]
         self.vz = velocity[2]
-    
+
     def get_intersect_pos_xy(self, other: HailStone) -> tuple[int, int]:
         """
         Returns the "intersection" position of this hailstone with another one (disregarding z).
@@ -21,13 +22,15 @@ class HailStone():
         # full credit to https://www.reddit.com/r/adventofcode/comments/18pnycy/2023_day_24_solutions/ket7ajw/?context=3
         if self.vy*other.vx == other.vy*self.vx:
             return False
-    
-        t_self = (other.vy*(self.x-other.x) - other.vx*(self.y - other.y)) / (self.vy*other.vx - other.vy*self.vx)
-        t_other = (self.vy*(other.x-self.x) - self.vx*(other.y - self.y)) / (other.vy*self.vx - self.vy*other.vx)
+
+        t_self = (other.vy*(self.x-other.x) - other.vx *
+                  (self.y - other.y)) / (self.vy*other.vx - other.vy*self.vx)
+        t_other = (self.vy*(other.x-self.x) - self.vx*(other.y -
+                   self.y)) / (other.vy*self.vx - self.vy*other.vx)
 
         if t_self <= 0 or t_other <= 0:
-            return None 
-        
+            return None
+
         return (self.x + t_self * self.vx, self.y + t_self * self.vy)
 
 
@@ -49,6 +52,7 @@ def count_all_intersections_inside_area(hailstones: list[HailStone], boundaries_
                     count += 1
     return count
 
+
 def get_all_intersect_hailstone(hailstones: list[HailStone]) -> HailStone:
     """
     Returns one hailstone that has an intersection on its path with all other given hailstones
@@ -64,11 +68,12 @@ def get_all_intersect_hailstone(hailstones: list[HailStone]) -> HailStone:
         z3_solver.add(x_sol + t * xv_sol == hailstone.x + t * hailstone.vx)
         z3_solver.add(y_sol + t * yv_sol == hailstone.y + t * hailstone.vy)
         z3_solver.add(z_sol + t * zv_sol == hailstone.z + t * hailstone.vz)
-    
+
     z3_solver.check()
     model = z3_solver.model()
-    
+
     return HailStone((model[x_sol].as_long(), model[y_sol].as_long(), model[z_sol].as_long()), (model[xv_sol].as_long(), model[yv_sol].as_long(), model[zv_sol].as_long()))
+
 
 def sum_pos_all_intersect_hailstone(hailstones: list[HailStone]) -> int:
     """
@@ -78,17 +83,18 @@ def sum_pos_all_intersect_hailstone(hailstones: list[HailStone]) -> int:
     """
     all_intersect_hailstone = get_all_intersect_hailstone(hailstones)
     return all_intersect_hailstone.x + all_intersect_hailstone.y + all_intersect_hailstone.z
-    
+
+
 if __name__ == "__main__":
     hailstones = []
     with open("input.txt") as fp:
-        while line:= fp.readline():
+        while line := fp.readline():
             pos, velocity = line.strip().split(" @ ")
             pos = pos.split(", ")
             pos = tuple(int(p) for p in pos)
             velocity = velocity.split(", ")
             velocity = tuple(int(v) for v in velocity)
             hailstones.append(HailStone(pos, velocity))
-    print(f"Part 1 Result: {count_all_intersections_inside_area(hailstones, ((200000000000000, 400000000000000), (200000000000000, 400000000000000)))}")
-    print(f"Part 2 Result: {get_all_intersect_hailstone(hailstones)}")
             
+    print(f"Part 1 Result: {count_all_intersections_inside_area(hailstones, ((200000000000000, 400000000000000), (200000000000000, 400000000000000)))}")
+    print(f"Part 2 Result: {sum_pos_all_intersect_hailstone(hailstones)}")
